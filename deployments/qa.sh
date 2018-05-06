5ade267db88b415272f54e405efc668c8bc504f7
David Rivero Meneses <davidriverome@gmail.com>
2018-05-06 03:33:02 -0300

    So hard in the beginning

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -3,6 +3,7 @@
 whoami
 sudo npm install -g now
 echo "deploying..."
+now --docker --public -t $NOW_TOKEN
 URL=$(now --docker --public -t $NOW_TOKEN)
 #echo "running acceptance on $URL"
 #curl --silent -L $URL
