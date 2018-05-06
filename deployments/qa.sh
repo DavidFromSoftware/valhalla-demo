eb8840080442de8d83db7801143661e92261d956
David Rivero Meneses <davidriverome@gmail.com>
2018-05-06 02:56:07 -0300

    try fix

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 whoami
-npm install -g now
-echo 'Deploying...'
-URL=$(now --docker -t $NOW_TOKEN)
-echo 'running acceptance on $URL'
+sudo npm install -g now
+echo "deploying..."
+URL=$(now --docker --public -t $NOW_TOKEN)
+echo "running acceptance on $URL"
 curl --silent -L $URL
