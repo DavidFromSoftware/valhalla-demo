d341ea716115fab0abd62cbd5212a484cbcd53a7
David Rivero Meneses <davidriverome@gmail.com>
2018-06-16 23:24:14 -0300

    correccion prod.sh

--- deployments/prod.sh
+++ deployments/prod.sh
@@ -1,4 +1,4 @@
 wget -O packer.zip https://releases.hashicorp.com/packer/1.2.4/packer_1.2.4_linux_amd64.zip
 unzip packer.zip
-./packer validates deployments/template.json
+./packer validate deployments/template.json
 ./packer build deployments/template.json
