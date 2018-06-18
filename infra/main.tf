9e5f74b5f272caa34f1f26519a7554cb3e188782
David Rivero Meneses <davidriverome@gmail.com>
2018-06-17 23:45:26 -0300

    Full deployment pipeline

--- infra/main.tf
+++ infra/main.tf
@@ -7,10 +7,16 @@ resource "digitalocean_droplet" "valhalla_web" {
   count  = 2
 	name   = "valhalla-qa${count.index+1}"
 	size   = "512mb"
-	image  = "35350781"
+	image  = "${var.image_id}"
 	region = "nyc1"
 	ssh_keys = [19215830]
   tags    = ["${digitalocean_tag.valhalla_tag.id}"]
+  lifecycle {
+    create_before_destroy = true
+  }
+  provisioner "local-exec" {
+    command = "sleep 160 && curl ${self.ipv4_address}:3000"
+  }
 	user_data = <<-EOF
   #cloud-config
   runcmd:
